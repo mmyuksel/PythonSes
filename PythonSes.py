f296e6ae9498d04e97de9270bc647c437a6fdb56
Murat <murax48@hotmail.com>
2015-06-26 20:09:04 +0300

    Create PythonSes.py

---
+++ PythonSes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys
+import urllib2
+import json
+#try:
+#    filename = sys.argv[1]
+#except IndexError:
+#    print 'Usage: transcribe.py <file>'
+#    sys.exit(1)
+
+filename = 'c:\asd.wav'
+
+f = open(filename)
+data = f.read()
+f.close()
+
+req = urllib2.Request('https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US', data=data, headers={'Content-type': 'audio/x-flac; rate=16000'})
+
+try:
+    ret = urllib2.urlopen(req)
+except urllib2.URLError:
+    print ("Servis Hatasi")
+    sys.exit(1)
+
+resp = ret.read()
+text = json.loads(resp)['hypotheses'][0]['utterance']
+print (text)
